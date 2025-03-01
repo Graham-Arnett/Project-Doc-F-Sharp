8e8b71330a88cfcc69a67f65e066b377e3976a60
Graham Arnett <GrahamKentArnett@outlook.com>
2025-03-01 14:07:39 -0500

    added first chapter2 assignment

---
+++ Chapter 2/StudentRegistrationApp/StudentRegistrationApp/Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "What is your first name? "
+let firstName = System.Console.ReadLine()
+
+printfn "What is your last name? "
+let lastName = System.Console.ReadLine()
+
+printfn "What is your year of birth? "
+let birthYear = System.Console.ReadLine()
+
+printfn $"Welcome {firstName} {lastName}!"
+
+printfn "Your registration is complete."
+
+printfn $"Your temporary password is: {lastName}{birthYear}"
