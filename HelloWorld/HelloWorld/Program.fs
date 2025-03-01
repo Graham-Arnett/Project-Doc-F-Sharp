11b16806f9014625fedc54a92da70ad0a8f8fb13
Graham Arnett <GrahamKentArnett@outlook.com>
2025-02-28 19:47:35 -0500

    init commit, hello world added

---
+++ HelloWorld/HelloWorld/Program.fs
@@ -0,0 +1,6 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+printfn "Hello World"
+printf "... and hello again!"
+
+printf "ah I see, printfn is basically the equivalent of going consolewrite vs consolewriteline"
